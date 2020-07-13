298e36d3b5c1edfcf95cc3525d91b40cb0584536
Doktor-Doom <andre.o.martin@gmail.com>
2020-07-13 15:09:12 -0700

    save multiply question 3???

--- class-05/demos/201d65-lab5/app.js
+++ class-05/demos/201d65-lab5/app.js
@@ -55,11 +55,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var productNumbers = a * b;
+  var string = 'The product of ' + a + ' and ' + b + ' is ' + productNumbers + '.';
+  return [productNumbers, string];
 }
 
+//The sum of ' + a +' and ' + b +' is ' +theSum +'.';
+
 // Here is the test for multiply(); uncomment it to run it
-//testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -76,7 +80,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sumOfThree = 
+  return [sumOfThree, ]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
